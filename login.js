94c383fa4608bde78e8eb59672edc6df90820a1c
Poojamadule <poojamadule25@gmail.com>
2022-12-01 16:16:23 +0530

    prime & palindrome problem

---
+++ login.js
@@ -0,0 +1,24 @@
+ let str="naman";
+let new_str="";
+for(i=str.length;i<=0;i--){
+new_str+=str[i]
+}
+if(str==new_str){
+console.log("Yes")
+}else{
+console.log("No")
+}
+
+
+let num=13;
+let count=0;
+for(i=0;i<num.length;i++){
+if(num%i==0){
+count++
+}
+if(count==0){
+console.log("Yes")
+}else{
+console.log("No")
+}
+
